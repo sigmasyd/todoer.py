1781739e97dccc36df1b9bb2e3bf92a4af84cbb0
Carlos Barcos <fbarcos8@hotmail.com>
2020-11-25 17:47:51 -0500

    Implementando la funcion crear
    	nuevo archivo:  todo/templates/todo/create.html
    	nuevo archivo:  todo/templates/todo/update.html
    	modificado:     todo/todo.py

--- todo/todo.py
+++ todo/todo.py
@@ -1,5 +1,5 @@
 from flask import (
-  Blueprint, flash, g, render_template, request, url_for
+  Blueprint, flash, g, render_template, request, url_for, redirect
 )
 from werkzeug.exceptions import abort
 from todo.auth import login_required
@@ -12,7 +12,7 @@ bp = Blueprint('todo',__name__)
 def index():
   db,c =get_db()
   c.execute(
-    'select t.id,t.description,u.username,t.completed,t.created_At from todo t join user u on t.created_by = u.id order by created_at desc'
+    'select t.id,t.description,u.username,t.completed,t.created_at from todo t join user u on t.created_by = u.id order by created_at desc'
   )
   todos = c.fetchall()
   return render_template('todo/index.html',todos=todos)
@@ -20,9 +20,24 @@ def index():
 @bp.route('/create',methods=['GET','POST'])
 @login_required
 def create():
-  return ''
+  if request.method == 'POST':
+    description = request.form['description']
+    error = None
+    if not description:
+      error = 'Descripción es requerida'
+    if error is not None:
+      flash(error)
+    else:
+      db,c = get_db()
+      c.execute(
+        'insert into todo (description, completed, created_by)'
+        ' values (%s,%s,%s)',(description,False,g.user['id'])
+      )
+      db.commit()
+      return redirect(url_for('todo.index'))
+  return render_template('todo/create.html')
 
 @bp.route('/update',methods=['GET','POST'])
 @login_required
 def update():
-  return ''
+  return render_template('todo/update.html')
