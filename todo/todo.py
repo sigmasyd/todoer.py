b314110eacc3fb827fad9a173052eeac0cd0d64c
Carlos Barcos <fbarcos8@hotmail.com>
2020-11-25 18:23:28 -0500

    Implementando el metodo update
    	modificado:     todo/templates/todo/update.html
    	modificado:     todo/todo.py

--- todo/todo.py
+++ todo/todo.py
@@ -37,7 +37,41 @@ def create():
       return redirect(url_for('todo.index'))
   return render_template('todo/create.html')
 
-@bp.route('/update',methods=['GET','POST'])
+def get_todo(id):
+  db,c = get_db()
+  c.execute(
+    'select t.id,t.description,t.completed,t.created_by,t.created_at,u.username'
+    ' from todo t join user u on t.created_by = u.id where t.id = %s',(id,)
+  )
+  todo = c.fetchone()
+  if todo is None:
+    abort(404, "El todo de id {0} no existe".format(id))
+  return todo
+
+@bp.route('/<int:id>/update',methods=['GET','POST'])
 @login_required
-def update():
-  return render_template('todo/update.html')
+def update(id):
+  todo = get_todo(id)
+  if request.method == 'POST':
+    description = request.form['description']
+    completed = True if request.form.get('completed') == 'on' else False
+    error = None
+
+    if not description:
+      error = 'Descripción es requerida'
+    if error is not None:
+      flash(error)
+    else:
+      db,c = get_db()
+      c.execute(
+        'update todo set description = %s, completed = %s'
+        ' where id = %s', (description,completed,id)
+      )
+      db.commit()
+      return redirect(url_for('todo.index'))
+  return render_template('todo/update.html',todo=todo)
+
+@bp.route('/<int:id>/delete',methods=['POST'])
+@login_required
+def delete():
+  return ''
